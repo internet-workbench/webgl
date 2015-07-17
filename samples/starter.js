40422e9fee84586741d9e03a1db6bc9b67c2235b
Bill Donahue <williamjohndonahue@gmail.com>
2015-07-17 17:22:30 +1200

    views

--- samples/starter.js
+++ samples/starter.js
@@ -3,6 +3,7 @@ define([
         'git/libs/master/threejs.org/detector/1.0.0/detector-1.0.0',
         'git/libs/master/threejs.org/stats/1.0.0/stats-1.0.0'
        ], function(THREE, Detector, Stats) {
+	
 	console.log("loaded webgl starter");
 	
 	var camera, scene, renderer;
