ba9cadcfe4ca134d3cdd89e28dc5d04b139fd98b
Bill Donahue <williamjohndonahue@gmail.com>
2015-07-17 13:08:44 +1200

    webgl

--- samples/starter.js
+++ samples/starter.js
@@ -1,3 +1,7 @@
-define(['git/libs/master/threejs.org/three/7.1.0/three-7.1.0'], function(three) {
-	console.log("loaded starter: " + three);
+define([
+        'git/libs/master/threejs.org/three/7.1.0/three-7.1.0',
+        'git/libs/master/threejs.org/detector/1.0.0/detector-1.0.0',
+        'git/libs/master/threejs.org/stats/1.0.0/stats-1.0.0'
+       ], function(THREE, Detector, Stats) {
+	console.log("loaded webgl starter");
 });
